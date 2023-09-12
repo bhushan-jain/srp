d3fff9386544426161bb8b6669e829ab065709ef
Bhushan Jain <bhushan.jain@maersk.com>
2023-09-12 23:22:58 +0530

    Without SRP first commit

---
+++ src/GroceryShop.java
@@ -0,0 +1,88 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroceryShop {
+  private int itemId;
+  private String itemName;
+  private LocalDate expiryDate;
+  private int price;
+
+  private final List<GroceryShop> groceryList = new ArrayList<>();
+
+  public GroceryShop() {}
+
+  public GroceryShop(int itemId, String itemName, LocalDate expiryDate, int price) {
+    this.itemId = itemId;
+    this.itemName = itemName;
+    this.expiryDate = expiryDate;
+    this.price = price;
+  }
+
+  public void addItem(GroceryShop item) {
+    groceryList.add(item);
+  }
+
+  public void removeItem(int number) {
+    groceryList.remove(--number);
+  }
+
+  public void printItems() {
+    groceryList.forEach(System.out::println);
+  }
+
+  public void save(String fileName) throws FileNotFoundException {
+    try (PrintWriter printWriter = new PrintWriter(fileName)) {
+      groceryList.forEach(printWriter::println);
+    }
+  }
+
+  public int getItemId() {
+    return itemId;
+  }
+
+  public void setItemId(int itemId) {
+    this.itemId = itemId;
+  }
+
+  public String getItemName() {
+    return itemName;
+  }
+
+  public void setItemName(String itemName) {
+    this.itemName = itemName;
+  }
+
+  public LocalDate getExpiryDate() {
+    return expiryDate;
+  }
+
+  public void setExpiryDate(LocalDate expiryDate) {
+    this.expiryDate = expiryDate;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  @Override
+  public String toString() {
+    return "GroceryShop{"
+        + "itemId="
+        + itemId
+        + ", itemName='"
+        + itemName
+        + '\''
+        + ", expiryDate="
+        + expiryDate
+        + ", price="
+        + price
+        + '}';
+  }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+
+public class Main {
+  public static void main(String[] args) throws FileNotFoundException {
+    GroceryShop groceryShop = new GroceryShop();
+
+    GroceryShop item1 = new GroceryShop(1, "Bathing Soap", LocalDate.of(2023,12,12), 20);
+    GroceryShop item2 = new GroceryShop(2, "Hair oil", LocalDate.of(2025,1,7), 120);
+    GroceryShop item3 = new GroceryShop(3, "Sugar", LocalDate.of(2024,8,1), 40);
+
+    groceryShop.addItem(item1);
+    groceryShop.addItem(item2);
+    groceryShop.addItem(item3);
+
+    groceryShop.printItems();
+    groceryShop.save("GroceryList.txt");
+
+    groceryShop.removeItem(2);
+
+    System.out.println("*****************************");
+    groceryShop.printItems();
+  }
+}
